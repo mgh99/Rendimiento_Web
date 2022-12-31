9722537ef20a9e70dbe49eeee9ad2ca4aeec964b
María González Herrero <43043718+mgh99@users.noreply.github.com>
2022-12-31 13:54:58 +0100

    Create Firefox.py

---
+++ Firefox.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import time
+
+URL = "https://www.bodasorganizadas.es"
+
+# Iniciamos el navegador con Selenium
+driver = webdriver.Firefox()
+
+# Medimos el tiempo de carga completo de la página
+start = time.perf_counter()
+driver.get(URL)
+end = time.perf_counter()
+
+# Calculamos el tiempo que ha tardado en cargar la página
+elapsed_time = end - start
+
+# Mostramos el tiempo en pantalla
+print("Elapsed time: {:.2f} seconds".format(elapsed_time))
+
+# Cerramos el navegador
+driver.quit()
